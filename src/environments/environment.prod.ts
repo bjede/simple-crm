39d2e35f69ea5a1c97d4d0b0568e498fecdf0bec
Aldin <aldin.bijedic@gmx.at>
2022-03-20 23:16:52 +0100

    Added firebase enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'simple-crm-26ff1',
+    appId: '1:911637975799:web:355ba6ca0efe128eeef9f2',
+    storageBucket: 'simple-crm-26ff1.appspot.com',
+    apiKey: 'AIzaSyD_ce_NmmaQaE7CZYXUprgaoUHmTOKAFXg',
+    authDomain: 'simple-crm-26ff1.firebaseapp.com',
+    messagingSenderId: '911637975799',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'simple-crm-26ff1',
+    appId: '1:911637975799:web:355ba6ca0efe128eeef9f2',
+    storageBucket: 'simple-crm-26ff1.appspot.com',
+    apiKey: 'AIzaSyD_ce_NmmaQaE7CZYXUprgaoUHmTOKAFXg',
+    authDomain: 'simple-crm-26ff1.firebaseapp.com',
+    messagingSenderId: '911637975799',
+  },
   production: false
 };
 
