94e0cd4af12cc011a4eda356f7c334cf33da8977
Aldin <aldin.bijedic@gmx.at>
2022-03-20 23:16:03 +0100

    Create user-detail.component.ts

---
+++ src/app/user-detail/user-detail.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/models/user.class';
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.scss']
+})
+export class UserDetailComponent implements OnInit {
+
+  userId = '';
+  user: User = new User();
+
+  constructor(private route: ActivatedRoute, public firestore: AngularFirestore) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: any) => {
+      this.userId = paramMap.get('id');
+      console.log(this.userId);
+      this.getUser();
+      
+    });
+    
+  }
+
+  getUser() {
+    this.firestore
+    .collection('users')
+    .doc(this.userId)
+    .valueChanges()
+    .subscribe((user: any) => {
+      this.user = new User(user);
+      console.log(this.user);
+      
+    })
+  }
+
+  openAddressDialog() {
+
+  }
+
+  editMenu() {
+    
+  }
+
+}
