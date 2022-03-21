49dc2512bc87e4af731cf4db7f5f330f6dbae39b
Aldin <aldin.bijedic@gmx.at>
2022-03-21 23:12:55 +0100

    Update dialog-edit-address.component.ts

--- src/app/dialog-edit-address/dialog-edit-address.component.ts
+++ src/app/dialog-edit-address/dialog-edit-address.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialogRef } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-dialog-edit-address',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogEditAddressComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  userId: string;
+  loading: boolean = false;
+
+  constructor(public dialogRef: MatDialogRef<DialogEditAddressComponent>,
+    public firestore: AngularFirestore,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  saveAddress() {
+    this.loading = true;
+    this.firestore
+    .collection('users')
+    .doc(this.userId)
+    .update(this.user.toJSON())
+    .then(() => {
+      this.loading = false;
+      this.dialogRef.close();
+    })
   }
 
 }
